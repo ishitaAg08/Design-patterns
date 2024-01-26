0a90fc12f86de5d75e75ad78bdf8f6a3826133a1
ishitaAg08 <ishitaag08@gmail.com>
2024-01-26 16:40:23 +0530

    Thread unsafe singleton pattern

---
+++ SingletonDemo/SingletonDemo/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    // sealed : no child class can be made ie prevent class inheritance
+    public sealed class Singleton
+    {
+        // static variable to hold the reference to a single instance created
+        private static int counter = 0;
+        private static Singleton? instance = null;
+
+        // auto-property
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+
+        // object can be created within this class only
+        private Singleton() {
+            counter++;
+            Console.WriteLine("Counter value: "+ counter.ToString());
+        }
+        public void Print(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
