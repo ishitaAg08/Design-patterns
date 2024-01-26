85425335c9d70d673b8bce853edb06045106cc77
ishitaAg08 <64578556+ishitaAg08@users.noreply.github.com>
2024-01-26 16:43:42 +0530

    Thread unsafe singleton pattern (#1)

---
+++ SingletonDemo/SingletonDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿using SingletonDemo;
+Console.WriteLine("Let's learn singleton design patter - Thread unsafe");
+
+Singleton fromE1 = Singleton.GetInstance;
+fromE1.Print("Hello from E1");
+
+Singleton fromE2 = Singleton.GetInstance;
+fromE2.Print("Hello from E2");
